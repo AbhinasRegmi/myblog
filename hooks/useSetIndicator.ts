f86f5228e065099df95fed5968d32f424a0d2de7
abhinas regmi <aviregmi2018@gmail.com>
2024-05-01 08:44:50 +0545

    added a todo to fix indicator.

--- hooks/useSetIndicator.ts
+++ hooks/useSetIndicator.ts
@@ -8,6 +8,7 @@ import { useIndicatorContext } from "@/context/indicator-context";
 export function useSetIndicator(isPending: boolean){
     const {setIsPending} = useIndicatorContext();
 
+    //TODO: i think when different comps passes isPending problem occurs look into it later.
     useEffect(() => {
         setIsPending(isPending);
     }, [isPending])
